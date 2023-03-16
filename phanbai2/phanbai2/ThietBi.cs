0d02d6a768d7de9d96115e80fa713c9a8425e06e
Thlinhh <128048225+Thlinhh@users.noreply.github.com>
2023-03-16 19:27:07 +0700

    Add files via upload

---
+++ phanbai2/phanbai2/ThietBi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phanbai2
+{
+    internal class ThietBi
+    {
+        public string MaThietBi { get; set; }
+        public string TenThietBi { get; set; }
+        public string NuocSanXuat { get; set; }
+        public int DonGia { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
